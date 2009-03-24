a8bffbc98f68926f7e1f32d3806bb3fe341dd495
Witold Firlej <git@grizz.pl>
2009-03-24 11:38:05 +0100

    README markdown syntax, some variables in main file

--- src/mirlight_gui.py
+++ src/mirlight_gui.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#mirlight ver. 0.01 by grizz - Witek Firlej http://grizz.pl
+
+__author__    = "Witold Firlej (http://grizz.pl)"
+__version__   = "0.01"
+__license__   = "GPLv2"
+__copyright__ = "Witold Firlej"
 
 import sys, ConfigParser
 from PyQt4 import QtCore, QtGui
