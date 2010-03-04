54e635a795f93beaf942412aca816a409650efae
Witek Firlej <git@grizz.pl>
2010-03-04 16:48:35 +0100

    setup_win_exr.py added

---
+++ src/setup_win_exe.py
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#mirlight by grizz - Witek Firlej http://grizz.pl
+# Copyright (C) 2009-2010 Witold Firlej
+
+from distutils.core import setup
+import py2exe
+
+# zipfile=None ## To create one large exe - uncoment this, and use --bundle 1 option
+
+setup(windows=[{"script" : "mirlight_gui.py"}],options={"py2exe":{"includes":["sip"]}})
