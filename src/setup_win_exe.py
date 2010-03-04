991931c22207f9fec2a5021c2f7f8b90992e5faa
Witek Firlej <git@grizz.pl>
2010-03-04 16:54:36 +0100

    some info about building .exe file

--- src/setup_win_exe.py
+++ src/setup_win_exe.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 #mirlight by grizz - Witek Firlej http://grizz.pl
 # Copyright (C) 2009-2010 Witold Firlej
+#
+# To build an .exe file use: python setup_win_exe.py py2exe --includes sip
 
 from distutils.core import setup
 import py2exe
